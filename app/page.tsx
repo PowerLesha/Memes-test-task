3a84156f6af17e5f4ee54a741f52d8339ceb8665
Lesha <leshapowerlifter@gmail.com>
2025-04-15 13:37:12 +0300

    added home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,55 +1,23 @@
-import { Link } from "@heroui/link";
-import { Snippet } from "@heroui/snippet";
-import { Code } from "@heroui/code";
-import { button as buttonStyles } from "@heroui/theme";
-
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
-
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>Make&nbsp;</span>
-        <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
-        <br />
-        <span className={title()}>
-          websites regardless of your design experience.
-        </span>
-        <div className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
-        </div>
-      </div>
+    <section className="flex flex-col items-center justify-center gap-8 py-12 px-4 text-center">
+      <h2 className="text-sm leading-relaxed w-full max-w-xl">
+        <span className="block mb-1">Are you scared?</span>
+        <span className="block mb-1">Don't be afraid</span>
+        <span className="block mb-1">I’m a friend, I won’t hurt you</span>
+        <span className="block mb-1">Come here, come to me</span>
+        <span className="block mb-1">Sit next to me</span>
+        <span className="block">Look at my memes</span>
+      </h2>
 
-      <div className="flex gap-3">
-        <Link
-          isExternal
-          className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
-          })}
-          href={siteConfig.links.docs}
-        >
-          Documentation
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
-      </div>
-
-      <div className="mt-8">
-        <Snippet hideCopyButton hideSymbol variant="bordered">
-          <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
-          </span>
-        </Snippet>
+      <div className="relative">
+        <img
+          src="https://i.ibb.co/dsZhXkPR/13-11-3-1280x1280.jpg"
+          alt="Meme Image"
+          width={200}
+          height={200}
+          className="rounded-xl shadow-lg"
+        />
       </div>
     </section>
   );
