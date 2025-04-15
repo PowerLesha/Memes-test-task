98902cca7eb4fbb9054ade99731d655f2cce6e7b
Lesha <leshapowerlifter@gmail.com>
2025-04-15 19:48:34 +0300

    some fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
       <head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1"
+          content="width=device-width, initial-scale=1, maximum-scale=0.8"
         />
       </head>
 
